6a295b041a333ab513512071b2e30a8a76268aa0
Aleks <javaenginee@gmail.com>
2019-11-19 22:30:03 +0200

    Localization Front-End Init

---
+++ config/localization-js.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+     * Set the names of files you want to add to generated javascript.
+     * Otherwise all the files will be included.
+     *
+     * 'messages' => [
+     *     'validation',
+     *     'forum/thread',
+     * ],
+     */
+    'messages' => [
+        'validation',
+        'pagination',
+        'auth'
+    ],
+
+    /*
+     * The default path to use for the generated javascript.
+     */
+    'path' => public_path('messages.js'),
+];
+
+
+/* ____ ! ! !  from /vendor/mariuzzo/laravel-js-localization/src/config/config.php */
